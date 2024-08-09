2004f35769756b0a43b23ed0008324cb8ba8cc85
Skidan Vladislav <139117484+TheDanskiSon09@users.noreply.github.com>
2024-08-09 15:29:17 +0300

    Add hider_checker.py file
    Adding hider_checker.py file, which defines regular users and administrators

---
+++ hiders_checker.py
@@ -0,0 +1,10 @@
+"""The module contains the hiders checker for the bot. """
+
+from hammett.conf import settings
+from hammett.core.hiders import HidersChecker
+
+
+class SchoolTaskerBotHiderChecker(HidersChecker):
+    async def is_admin(self, update, _context) -> bool:
+        user = update.effective_user
+        return str(user.id) in settings.ADMIN_GROUP
