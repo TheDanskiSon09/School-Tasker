eccd690cc2565f1e37615f131e94457b88104b3a
Skidan Vladislav <139117484+TheDanskiSon09@users.noreply.github.com>
2024-08-09 16:58:11 +0300

    Remove unused data
    Add updated version without unused data

--- settings.py
+++ settings.py
@@ -5,4 +5,3 @@ HIDERS_CHECKER = 'hiders_checker.SchoolTaskerBotHiderChecker'
 TOKEN = os.getenv("TOKEN", "")
 ADMIN_GROUP = os.getenv("ADMIN_GROUP", "").split(",")
 DIRECTOR_ID = os.getenv("DIRECTOR_ID", "")
-SAVE_LATEST_MESSAGE = True
