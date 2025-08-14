83f0482ac6f48772aab30d4c8f1a68feb5c196bf
Skidan Vladislav <139117484+TheDanskiSon09@users.noreply.github.com>
2025-08-14 14:17:47 +0300

    Delete base directory

--- base/base_screen.py
+++
@@ -1,8 +0,0 @@
-from hammett.core import Screen
-from hammett.conf import settings
-
-
-class BaseScreen(Screen):
-    cache_covers = True
-    hide_keyboard = True
-    cover = str(settings.MEDIA_ROOT) + '/' + 'logo.webp'
